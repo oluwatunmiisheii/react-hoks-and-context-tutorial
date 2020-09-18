97e8bbdb87b8e50ee36d2d142896f525134a9b5b
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2020-09-18 23:15:50 +0100

    feat: using static to access context daata in class component

---
+++ app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import BookList from './components/BookList'
+import ThemeContextProvider from './context/ThemeContext';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
