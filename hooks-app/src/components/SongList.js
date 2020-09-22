4254d9badf25c9ed686aad9a82021a0db511782c
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2020-09-22 16:50:40 +0100

    feat: useState react hook

--- hooks-app/src/components/SongList.js
+++ hooks-app/src/components/SongList.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { getKey } from './../util/getKey';
 
 const SongList = () => {
-  const songsArr = [
+  const initState = [
     { title: 'almost home', id: getKey() },
     { title: 'memory gospel', id: getKey() },
     { title: 'the wild wilderness', id: getKey() }
@@ -10,7 +10,7 @@ const SongList = () => {
   const addSongHandler = () => {
     setSongs([...songs, { title: 'new song', id: getKey() }])
   }
-  const [songs, setSongs] = useState(songsArr)
+  const [songs, setSongs] = useState(initState)
   return (
     <div className="song-list">
       <ul>
