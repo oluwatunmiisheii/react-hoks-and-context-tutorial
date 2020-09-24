d1dab6089908a91c9d2c964451d24caf55761d26
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2020-09-24 01:04:14 +0100

    feat: created books context and used useReducer hook

---
+++ reading-list/src/context/BookContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useReducer, useEffect } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
+
+
+export const BookContext = createContext()
+
+const BookContextProvider = ({ children }) => {
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('books')
+    return localData ? JSON.parse(localData) : []
+  })
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books))
+  }, [books])
+  return (
+    <BookContext.Provider value={{ books, dispatch }}>
+      {children}
+    </BookContext.Provider>
+  )
+}
+export default BookContextProvider
